54689e107288bd86f694fd819751a8d2c0a2895b
brosing <singgihnn@gmail.com>
2023-01-09 12:05:29 +0700

    test

---
+++ pages/pokemon-hook.tsx
@@ -0,0 +1,81 @@
+import { Box, Button, Card, CardBody, Text, Image } from "@chakra-ui/react";
+import { useState } from "react";
+
+import { Pokemon } from "../model";
+
+const getURL = () => {
+  const random = Math.floor(Math.random() * 99) + 1;
+  return `https://pokeapi.co/api/v2/pokemon/${random}`;
+};
+
+const useRandomPokemon = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+
+  const randomizer = async () => {
+    setLoading(true);
+
+    fetch(getURL())
+      .then((res) => res.json())
+      .then((res: Pokemon) => {
+        setPokemon(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return {
+    loading,
+    pokemon,
+    randomizer,
+  };
+};
+
+const RandomPokemon = ({ pokemon }: { pokemon: Pokemon | null }) => (
+  <>
+    <Text fontSize="2xl" fontWeight="medium" align="center">
+      {pokemon?.name.toUpperCase() ?? "POKEBALL"}
+    </Text>
+
+    <Image
+      src={
+        pokemon?.sprites.other?.["official-artwork"].front_default ??
+        "/pokeball.png"
+      }
+      alt="Pokeball"
+      width={360}
+      height={360}
+    />
+  </>
+);
+
+export default function Home() {
+  const { loading, pokemon, randomizer } = useRandomPokemon();
+
+  return (
+    <Box h="100vh" display="flex" justifyContent="center" alignItems="center">
+      <Card shadow="2xl" size="lg">
+        <CardBody
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          gap={10}
+        >
+          <RandomPokemon pokemon={pokemon} />
+          <Button
+            colorScheme="red"
+            size="lg"
+            onClick={randomizer}
+            isLoading={loading}
+          >
+            SUMMON POKEMON
+          </Button>
+        </CardBody>
+      </Card>
+    </Box>
+  );
+}
